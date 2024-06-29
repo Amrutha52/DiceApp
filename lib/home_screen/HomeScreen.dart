5a344f2294aebb827aa6d22a011a9d256eaf53e5
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-06-29 16:08:49 +0530

    first commit

---
+++ lib/home_screen/HomeScreen.dart
@@ -0,0 +1,49 @@
+
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class  HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List dices = ["assets/images/d1.png"
+    ,"assets/images/d2.png"
+    ,"assets/images/d3.png"
+    ,"assets/images/d4.png"
+    ,"assets/images/d5.png"
+    ,"assets/images/d6.png"];
+  int? randomNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: InkWell(
+          onTap: (){
+            setState(() {
+              randomNumber = Random().nextInt(6) ;
+            });
+          },
+          child: SizedBox(
+            height: 200,
+            width: 200,
+            child: Image.asset(
+              fit: BoxFit.contain,
+              randomNumber == null ? "assets/images/button.png":
+              dices[randomNumber!],
+            ),
+          ),
+        )
+      ),
+    );
+  }
+
+
+}
